adffb7c559343b1cafd3a5f81ed7964585bf6896
Eduardo Kurek <eduardokurek2003@gmail.com>
2023-12-16 00:36:32 -0300

    Cabeçalhos basicos de OwnerDAO

--- Trabalho final/codigo/src/DAO/OwnerDAO.h
+++ Trabalho final/codigo/src/DAO/OwnerDAO.h
@@ -2,10 +2,18 @@
 
 #include "../Model/Owner.h"
 #include <string>
+#include <vector>
 
 class OwnerDAO{
 
-    public:
+private:
+    std::vector<Owner*> owners;
+
+public:
     Owner* create(std::string nome, std::string email, std::string senha);
+    std::vector<Owner*> retrieve();
+    Owner* retrieve(std::string ownerId);
+    void update(Owner* owner);
+    void remove(Owner* owner);
 
 };
